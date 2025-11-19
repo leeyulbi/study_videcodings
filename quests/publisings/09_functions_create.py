04ce6921925d17078c7a513546a5899349d75ba1
leeyulbi <egoism030400@gmail.com>
2025-11-19 17:02:30 +0900

    6

--- quests/publisings/09_functions_create.py
+++ quests/publisings/09_functions_create.py
@@ -1,4 +1,4 @@
-섭씨 온도 3개 평균 반환 함수
+# 섭씨 온도 3개 평균 반환 함수
 def avg_celsius(t1, t2, t3):
     return (t1 + t2 + t3) / 3
 
@@ -7,7 +7,7 @@ print(avg_celsius(10, 20, 30))  # 20.0
 print(avg_celsius(0, 5, 10))    # 5.0
 print(avg_celsius(-5, 5, 15))   # 5.0
 
-이름 + 좋아하는 언어 2개 출력 함수
+# 이름 + 좋아하는 언어 2개 출력 함수
 def print_fav_lang(name, lang1, lang2):
     print(f"{name}님의 선호 언어는 {lang1}, {lang2} 입니다.")
 
@@ -16,7 +16,7 @@ print_fav_lang("홍길동", "Python", "Java")
 print_fav_lang("이율비", "JavaScript", "Python")
 print_fav_lang("김철수", "C", "Go")
 
-점수 리스트에서 60점 이상만 합산하는 함수
+# 점수 리스트에서 60점 이상만 합산하는 함수
 def sum_pass_scores(scores):
     total = 0
     for s in scores:
@@ -29,7 +29,7 @@ print(sum_pass_scores([50, 60, 70]))       # 130
 print(sum_pass_scores([100, 20, 30, 90]))  # 190
 print(sum_pass_scores([59, 58, 57]))       # 0
 
-문자열 두 개를 이어 붙이는 함수
+# 문자열 두 개를 이어 붙이는 함수
 def combine(str1, str2):
     return str1 + str2
 
@@ -39,7 +39,7 @@ print(combine("율빛", "힐링센터"))        # 율빛힐링센터
 print(combine("GREEUM ", "PDRN"))        # GREEUM PDRN
 
 
-온도 리스트 → 모두 섭씨로 변환하여 새 리스트 반환(Celsius = (F - 32) / 1.8)
+# 온도 리스트 → 모두 섭씨로 변환하여 새 리스트 반환(Celsius = (F - 32) / 1.8)
 def to_celsius_list(temp_list):
     result = []
     for f in temp_list:
